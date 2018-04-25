a81457312d616cabf5a8bc3f406c13880cae6806
klaszky <jfklaszky@gmail.com>
2018-04-25 17:43:54 -0400

    these functions should be enough to do the login stuff

---
+++ auth.py
@@ -0,0 +1,41 @@
+import hashlib, uuid
+
+def findUser(name):
+	with open("users.txt", "w+") as usrFile:
+		for line in usrFile:
+			line = line.split(" : ")
+			if line[0].lower == name:
+				return True
+			else:
+				continue
+		return False; 
+
+def checkPassword(name, pw):
+	with open("users.txt", "w+") as usrFile:
+		for line in usrFile:
+			line = line.split(" : ")
+			if line[0].lower == name:
+				if line[1] == pw:
+					return True
+				else:
+					return False
+			else:
+				continue
+		return False; 
+
+def addUser(name, hash, salt):
+	with open("users.txt", "a") as usrFile:
+		name = name.lower().strip()
+		usrFile.write(name + " : " + hash + " : " + salt)
+
+
+def hashPW(pw, salt):
+    return hashlib.sha512((pw + salt).encode('utf-8')).hexdigest()
+
+password = "temp"
+
+salt = uuid.uuid4().hex
+
+
+
+print(hashPW(password, salt))
