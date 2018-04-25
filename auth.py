2c0134c39a1773bb554a7755c9c74ad2046817c7
klaszky <jfklaszky@gmail.com>
2018-04-25 17:58:22 -0400

    fixed a minor error in my findUser and checkPassword functions.

--- auth.py
+++ auth.py
@@ -4,7 +4,8 @@ def findUser(name):
 	with open("users.txt", "r") as usrFile:
 		for line in usrFile:
 			line = line.split(" : ")
-			if line[0].lower == name:
+			print(line)
+			if line[0].lower() == name.lower():
 				return True
 			else:
 				continue
@@ -14,7 +15,8 @@ def checkPassword(name, pw):
 	with open("users.txt", "r") as usrFile:
 		for line in usrFile:
 			line = line.split(" : ")
-			if line[0].lower == name:
+			print(line)
+			if line[0].lower() == name.lower():
 				if line[1] == pw:
 					return True
 				else:
@@ -31,11 +33,3 @@ def addUser(name, hash, salt):
 
 def hashPW(pw, salt):
     return hashlib.sha512((pw + salt).encode('utf-8')).hexdigest()
-
-password = "temp"
-
-salt = uuid.uuid4().hex
-
-
-
-print(hashPW(password, salt))
