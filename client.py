527ec35718f8cf271dab25140215b6632bbde65e
klaszky <jfklaszky@gmail.com>
2018-04-26 10:43:04 -0400

    added some comment for the time being. I'll do more after I get back home

--- client.py
+++ client.py
@@ -8,10 +8,25 @@ import pprint
 
 MAX_SIZE = 1024
 
+##########
+## So this all seems to be working, but it's
+## pretty dependent on the user entering correct
+## input. I'll try to make it a little more fault
+## tolerant I have time.
+##########
+
+## Once a connection with the server is made we go right into
+## auth(). This sticks the user in a loop until they end the session
+## or the successfully make a new user account of authenticate
+## to the server. Also, we're sending the password over as it
+## at the moment because the ssl connection should encrypt it
+## in transit. Worse case: I can just hash it before
+## I send it over which would be a bit annoying, but not too hard.
+##########
 def auth(sslSock):
     data = sslSock.read(MAX_SIZE).decode()
 
-    print ("Client: recieved message {}".format(data))
+    print ("Client: received message {}".format(data))
 
     message = input("Reply: ")
 
@@ -19,7 +34,7 @@ def auth(sslSock):
 
     while message != "end" :
         data = sslSock.read(MAX_SIZE).decode()
-        print ("Client: recieved message {}".format(data))
+        print ("Client: received message {}".format(data))
         if data == "Success! You were added as a new User." or data == "Success!":
             break
         message = input("Reply: ")
@@ -28,12 +43,19 @@ def auth(sslSock):
 
     msgBoard(sslSock)
 
-
+## Main loop onces a client has been authenticated.
+## Checks the client's input and if it matches a command
+## that we support we run the appropriate function or
+## end the session. Not fault tolerant at the moment though.
+## I need to do some error checking on the input and need to
+## handle being given something that we don't have a command
+## for.
+##########
 def msgBoard(sslSock):
     print ("List of boards:")
     sslSock.write(("listB").encode('utf-8'))
     data = sslSock.read(MAX_SIZE).decode()
-    print ("Client: recieved message {}".format(data))
+    print ("Client: received message {}".format(data))
 
     message = input("Reply: ")
 
@@ -41,11 +63,13 @@ def msgBoard(sslSock):
 
     while message != "end" :
         data = sslSock.read(MAX_SIZE).decode()
-        print ("Client: recieved message {}".format(data))
+        print ("Client: received message {}".format(data))
         message = input("Reply: ")
         print ("Client: sending message: {}".format(message))
         sslSock.write(message.encode('utf-8'))
 
+## Makes a connection to the server on a user supplied port.
+##########
 def main():
     # parse all the arguments to the client and get the arguments into local variables
     parser = argparse.ArgumentParser(description='Computer Security Client')
@@ -61,7 +85,7 @@ def main():
     local_port =  args['local_port']
 
 
-    # create a socket, bind to local specified port, get serverdestination then connect to the server
+    # create a socket, bind to local specified port, get server destination then connect to the server
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.bind(('', int(local_port)))
 
--- server.py
+++ server.py
@@ -12,6 +12,19 @@ import time
 
 MAX_SIZE = 1024
 
+##########
+## TODO: Make a lot of my functions more
+## fault tolerant. I currently do no null 
+## checks which could be a huge issue is
+## files doesn't exist or they are empty
+##########
+
+
+
+## This might be combined with findGroup at some point
+## returns true is a given user name is in users.txt and false
+## otherwise.
+##########
 def findUser(name):
     with open("users.txt", "r") as usrFile:
         for line in usrFile:
@@ -22,6 +35,9 @@ def findUser(name):
                 continue
         return False;
 
+## Returns true is a given group name is in boards.txt and false
+## otherwise.
+##########
 def findGroup(name):
     with open("boards.txt", "r") as bFile:
         for line in bFile:
@@ -31,6 +47,9 @@ def findGroup(name):
                 continue
         return False;
 
+## Returns true if a given hashed and salted password is
+## associated with a given user name and false otherwise.
+##########
 def checkPassword(name, pw):
     with open("users.txt", "r") as usrFile:
         for line in usrFile:
@@ -44,15 +63,24 @@ def checkPassword(name, pw):
                 continue
         return False;
 
+## Takes a user name, a hashed and salted password
+## and the salt used as args and appends them to the
+## users.txt file.
+##########
 def addUser(name, hash, salt):
     with open("users.txt", "a") as usrFile:
         name = name.lower().strip()
         usrFile.write("\n" + name + " : " + hash + " : " + salt)
 
-
+## Takes a password and salt combines them and
+## does a sha 512 hash on them. Returns said hash.
+##########
 def hashPW(pw, salt):
     return hashlib.sha512((pw.strip() + salt).encode('utf-8')).hexdigest()
 
+## Just goes through the boards.txt file and spits out
+## a string with all the extant boards.
+##########
 def displayBoards():
     with open("boards.txt", "r+") as boardFile:
         toReturn = ""
@@ -60,6 +88,12 @@ def displayBoards():
             toReturn += line
         return toReturn
 
+## If the user entered "get ...." it splits up
+## their input and looks a group name that's the same
+## as whatever the user typed after get. 
+## I Think I still have some work to do on this function
+## and the post function to make the inputs more consistent.
+##########
 def getPosts(message):
     message = message.split(" ")
     if len(message) == 1:
@@ -80,6 +114,13 @@ def getPosts(message):
         else:
             return "Sorry, that group doesn't seem to exist"
 
+## Takes in a message that starts with post,
+## it then looks at the next word and checks if there
+## is a group with that name. If yes, it takes the rest
+## of the user input, timestamps it, and posts it to
+## that given group. If it doesn't find the group it
+## just apologized and moves on. 
+##########
 def postComment(message):
     message = message.split(" ")
     if len(message) < 2:
@@ -98,9 +139,20 @@ def postComment(message):
         else:
             return "Sorry, that group doesn't seem to exist"
 
+## TODO: Make this fucking thing.
+def addGroup(message):
+    pass
 
+## As soon as a connection is made with the client
+## authenticate() is run which will start by asking for
+## a user name. After that it looks for that user name in
+## the users.txt file. If found it asks for a password which
+## it then salts and hashes and checks it against the hash
+## in the users.txt file. If they match sends back a success
+## message. If the user isn't found it just for a password
+## and adds them as a new user.
+##########
 def authenticate(connection_stream):
-    #get username and password this is where we will deal with username an password
     retry = False
     while True:
         if not retry:
@@ -127,24 +179,34 @@ def authenticate(connection_stream):
             addUser(username, password, salt)
             connection_stream.send(("Success! You were added as a new User.").encode('utf-8'))
             return username
-
+## I hate the name of this function. I'm going to change it at 
+## some point. Bascially the main loop of the message board
+## onces some one is authenticated it allows them to: get
+## the messages from a given group, post a message to a group,
+## list the groups, and *TODO* add a group. The user can
+## end a session whenever they like with the end command.
+##########
 def deal_with_msg(connection_stream):
     # waiting for message
     while True:
         message = connection_stream.read(MAX_SIZE).decode()
-        print ('Server: recieved {}'.format(message))
-        if(message == "listB"):
+        print ('Server: received {}'.format(message))
+        if(message == "list"):
             connection_stream.send((displayBoards()).encode('utf-8'))
         elif(message.startswith("get".lower().strip())):
             connection_stream.send(getPosts(message).encode('utf-8'))
         elif(message.startswith("post".lower().strip())):
             connection_stream.send(postComment(message).encode('utf-8'))
+        elif(message.startswith("add".lower().strip())):
+            pass
         elif(message.startswith("end".lower().strip())):
             break
         #lets error check this later in case no get/post/end
         else:
             return
-
+## TODO: Make this mutli threaded.
+## Set up of ssl socket and waits for a client connection.
+##########
 def main():
     # parse arguments to the client
     parser = argparse.ArgumentParser(description='Computer Security Server')
